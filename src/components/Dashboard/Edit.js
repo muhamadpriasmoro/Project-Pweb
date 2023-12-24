7eca495dd0f9a871d85cbf4f07f41a7b497b4f7e
muhamadpriasmoro <143477826+muhamadpriasmoro@users.noreply.github.com>
2023-12-24 21:16:34 +0700

    Add files via upload

---
+++ src/components/Dashboard/Edit.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Edit = ({ employees, selectedEmployee, setEmployees, setIsEditing }) => {
+  const id = selectedEmployee.id;
+
+  const [firstName, setFirstName] = useState(selectedEmployee.firstName);
+  const [lastName, setLastName] = useState(selectedEmployee.lastName);
+  const [email, setEmail] = useState(selectedEmployee.email);
+  const [salary, setSalary] = useState(selectedEmployee.salary);
+  const [date, setDate] = useState(selectedEmployee.date);
+
+  const handleUpdate = e => {
+    e.preventDefault();
+
+    if (!firstName || !lastName || !email || !salary || !date) {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'Semua kolom harus diisi.',
+        showConfirmButton: true,
+      });
+    }
+
+    const employee = {
+      id,
+      firstName,
+      lastName,
+      email,
+      salary,
+      date,
+    };
+
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].id === id) {
+        employees.splice(i, 1, employee);
+        break;
+      }
+    }
+
+    localStorage.setItem('employees_data', JSON.stringify(employees));
+    setEmployees(employees);
+    setIsEditing(false);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Berhasil!',
+      text: `Data ${employee.firstName} ${employee.lastName} telah diperbarui.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleUpdate}>
+        <h1>Edit Pegawai</h1>
+        <label htmlFor="firstName">Nama Depan</label>
+        <input
+          id="firstName"
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Nama Belakang</label>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="salary">Gaji (Rp.)</label>
+        <input
+          id="salary"
+          type="number"
+          name="salary"
+          value={salary}
+          onChange={e => setSalary(e.target.value)}
+        />
+        <label htmlFor="date">Tanggal</label>
+        <input
+          id="date"
+          type="date"
+          name="date"
+          value={date}
+          onChange={e => setDate(e.target.value)}
+        />
+        <div style={{ marginTop: '30px' }}>
+          <input type="submit" value="Perbarui" />
+          <input
+            style={{ marginLeft: '12px' }}
+            className="muted-button"
+            type="button"
+            value="Batal"
+            onClick={() => setIsEditing(false)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Edit;
